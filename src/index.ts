bef9697383feb81a5b2821bb1acbaa18aa74dc7a
Aaron Leonard <aaron.michael.leonard@gmail.com>
2018-04-08 20:53:06 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,146 @@
+export interface IndexedField<M> {
+  [value: string]: Array<M> | IndexedField<M>;
+}
+
+export interface IndexedState<M> {
+  [field: string]: IndexedField<M>;
+}
+
+export interface IndexedMemoizableFunction<M> {
+  (...args: Array<any>): IndexedState<M>;
+}
+
+function ensureKeyOnObject<M>(
+  obj: IndexedField<M>,
+  key: string,
+  defaultValue: any
+) {
+  if (!obj.hasOwnProperty(key)) {
+    obj[key] = defaultValue;
+  }
+}
+
+function indexIntoObject<M>(
+  baseObject: IndexedField<M> | IndexedState<M>,
+  item: M,
+  field: string | Array<string>
+): IndexedField<M> {
+  let value: any, nextFields: Array<string>, currentField: string;
+
+  if (Array.isArray(field)) {
+    [currentField, ...nextFields] = field;
+  } else {
+    currentField = field;
+  }
+
+  value = item[currentField];
+
+  if (value === null) {
+    return;
+  }
+
+  if (Array.isArray(nextFields) && nextFields.length) {
+    ensureKeyOnObject(baseObject, value, {});
+    indexIntoObject(baseObject[value] as IndexedField<M>, item, nextFields);
+  } else {
+    ensureKeyOnObject(baseObject, value, []);
+    (baseObject[value] as Array<M>).push(item);
+  }
+}
+
+export const indexListByFields = (...fields: Array<string | string[]>) => <
+  M extends object
+>(
+  list: Array<M>
+): IndexedState<M> => {
+  // preinitialize all fields
+  let indexedBaseObj: IndexedState<M> = fields.reduce((obj: object, field) => {
+    if (typeof field === 'string') {
+      obj[field] = {};
+    } else if (Array.isArray(field)) {
+      obj[field.join('')] = {};
+    }
+    return obj;
+  }, {});
+
+  for (let i = 0, len = list.length; i < len; i++) {
+    let item = list[i];
+
+    for (let j = 0, subLen = fields.length; j < subLen; j++) {
+      let field = fields[j];
+
+      const key = Array.isArray(field) ? field.join('') : field;
+
+      indexIntoObject(indexedBaseObj[key], item, field);
+    }
+  }
+
+  return indexedBaseObj;
+};
+
+function isArrayEqual<T>(a: Array<T>, b: Array<T>) {
+  // step 1: if one is something and the other is nothing, clearly not the same
+  if ((!a && b) || (a && !b) || !a.length || !b.length) {
+    return false;
+  }
+
+  // step 2: if they are not the same length, clearly not the same
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  // step 3: if they contain different objects at the same index, clearly not the same
+  for (let i = a.length - 1; i >= 0; i--) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export const memoizeIndexedArray = <M>(fn: IndexedMemoizableFunction<M>) => {
+  let lastResult: IndexedState<M>;
+
+  return (...args: Array<any>) => {
+    let nextResult: IndexedState<M> = fn(...args);
+
+    if (lastResult) {
+      let outerChanged = false;
+
+      const nextIndexed: IndexedState<M> = Object.keys(nextResult).reduce(
+        (state, field) => {
+          let innerChanged = false;
+
+          let nextValueIndexedList = Object.keys(nextResult[field]).reduce(
+            (acc, value) => {
+              if (
+                isArrayEqual(lastResult[field][value] as Array<M>, nextResult[field][value] as Array<M>)
+              ) {
+                acc[value] = lastResult[field][value];
+              } else {
+                innerChanged = true;
+                acc[value] = nextResult[field][value];
+              }
+
+              return acc;
+            },
+            {}
+          );
+
+          state[field] = innerChanged
+            ? nextValueIndexedList
+            : lastResult[field];
+
+          outerChanged = innerChanged || outerChanged;
+
+          return state;
+        },
+        {}
+      );
+
+      return outerChanged ? (lastResult = nextIndexed) : lastResult;
+    } else {
+      return (lastResult = nextResult);
+    }
+  };
+};
---
+++ tests/index.spec.ts
@@ -0,0 +1,164 @@
+import { indexListByFields, memoizeIndexedArray } from '../src';
+
+const items = [
+  {
+    title: 'War of the Worlds',
+    authorId: 1,
+    publisherId: 1,
+    subjectId: 1,
+  },
+  {
+    title: 'Of Mice and Men',
+    authorId: 2,
+    publisherId: 1,
+    subjectId: 2,
+  },
+  {
+    title: 'The Time Machine',
+    authorId: 1,
+    publisherId: 2,
+    subjectId: 1,
+  },
+  {
+    title: 'Little Women',
+    authorId: 3,
+    publisherId: 3,
+    subjectId: 2,
+  },
+  {
+    title: 'The Scarlet Letter',
+    authorId: 4,
+    publisherId: 2,
+    subjectId: 3,
+  },
+  {
+    title: 'The Great Gatsby',
+    authorId: 5,
+    publisherId: 3,
+    subjectId: 3,
+  },
+  {
+    title: 'Adventures of Huckleberry Finn',
+    authorId: 6,
+    publisherId: 2,
+    subjectId: 2,
+  },
+  {
+    title: 'Adventures of Tom Sawyer',
+    authorId: 6,
+    publisherId: 3,
+    subjectId: 2,
+  },
+  {
+    title: 'Lord of the Flies',
+    authorId: 7,
+    publisherId: 4,
+    subjectId: 3,
+  },
+  {
+    title: 'The Invisible Man',
+    authorId: 1,
+    publisherId: 2,
+    subjectId: 1,
+  },
+];
+
+const newItem = {
+  title: 'Free Fall',
+  authorId: 7,
+  publisherId: 2,
+  subjectId: 3,
+};
+
+const WAROFTHEWORLDS = items[0];
+const OFMICEANDMEN = items[1];
+const THETIMEMACHINE = items[2];
+const LITTLEWOMEN = items[3];
+const THESCARLETLETTER = items[4];
+const THEGREATGATSBY = items[5];
+const HUCKLEBERRYFINN = items[6];
+const TOMSAWYER = items[7];
+const LORDOFTHEFLIES = items[8];
+const THEINVISIBLEMAN = items[9];
+
+describe('indexListByFields', () => {
+  const indexer = indexListByFields('authorId', 'publisherId', 'subjectId', [
+    'publisherId',
+    'subjectId',
+  ]);
+  test('it produces the expected result', () => {
+    const itemsIndexedBy = indexer(items);
+
+    expect(itemsIndexedBy).toEqual({
+      authorId: {
+        1: [WAROFTHEWORLDS, THETIMEMACHINE, THEINVISIBLEMAN],
+        2: [OFMICEANDMEN],
+        3: [LITTLEWOMEN],
+        4: [THESCARLETLETTER],
+        5: [THEGREATGATSBY],
+        6: [HUCKLEBERRYFINN, TOMSAWYER],
+        7: [LORDOFTHEFLIES],
+      },
+      publisherId: {
+        1: [WAROFTHEWORLDS, OFMICEANDMEN],
+        2: [THETIMEMACHINE, THESCARLETLETTER, HUCKLEBERRYFINN, THEINVISIBLEMAN],
+        3: [LITTLEWOMEN, THEGREATGATSBY, TOMSAWYER],
+        4: [LORDOFTHEFLIES],
+      },
+      subjectId: {
+        1: [WAROFTHEWORLDS, THETIMEMACHINE, THEINVISIBLEMAN],
+        2: [OFMICEANDMEN, LITTLEWOMEN, HUCKLEBERRYFINN, TOMSAWYER],
+        3: [THESCARLETLETTER, THEGREATGATSBY, LORDOFTHEFLIES],
+      },
+      publisherIdsubjectId: {
+        1: {
+          1: [WAROFTHEWORLDS],
+          2: [OFMICEANDMEN],
+        },
+        2: {
+          1: [THETIMEMACHINE, THEINVISIBLEMAN],
+          2: [HUCKLEBERRYFINN],
+          3: [THESCARLETLETTER],
+        },
+        3: {
+          2: [LITTLEWOMEN, TOMSAWYER],
+          3: [THEGREATGATSBY],
+        },
+        4: {
+          3: [LORDOFTHEFLIES],
+        },
+      },
+    });
+  });
+});
+
+describe('memoizeIndexedArray', () => {
+  const indexer = memoizeIndexedArray(indexListByFields('authorId', 'publisherId', 'subjectId', [
+    'publisherId',
+    'subjectId',
+  ]));
+
+  test('it produces the expected result', () => {
+    const itemsIndexedBy = indexer(items);
+    const itemsIndexedByNext = indexer([...items, newItem]);
+
+    expect(itemsIndexedBy.authorId[1]).toBe(itemsIndexedByNext.authorId[1]);
+    expect(itemsIndexedBy.authorId[4]).toBe(itemsIndexedByNext.authorId[4]);
+    expect(itemsIndexedBy.authorId[7]).not.toBe(itemsIndexedByNext.authorId[7]);
+
+    expect(itemsIndexedBy.publisherId[1]).toBe(itemsIndexedByNext.publisherId[1]);
+    expect(itemsIndexedBy.publisherId[2]).not.toBe(itemsIndexedByNext.publisherId[2]);
+
+    expect(itemsIndexedBy.subjectId[1]).toBe(itemsIndexedByNext.subjectId[1]);
+    expect(itemsIndexedBy.subjectId[3]).not.toBe(itemsIndexedByNext.subjectId[3]);
+  });
+
+  test('it returns the same state when no changes', () => {
+    const itemsIndexedBy = indexer(items);
+    const itemsIndexedByNext = indexer(items);
+
+    expect(itemsIndexedBy.authorId[1]).toBe(itemsIndexedByNext.authorId[1]);
+    expect(itemsIndexedBy.publisherId[1]).toBe(itemsIndexedByNext.publisherId[1]);
+    expect(itemsIndexedBy).toBe(itemsIndexedByNext);
+  });
+});
